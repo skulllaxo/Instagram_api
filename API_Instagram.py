adaded637b852b40b73ac519cba0d3ef5f951914
skulllaxo <47420789+skulllaxo@users.noreply.github.com>
2019-04-09 11:15:01 -0300

    Add files via upload

---
+++ API_Instagram.py
@@ -0,0 +1,20 @@
+import facebook
+import requests
+
+import Post_info as pi
+import Page_instagram as pgi
+import Insights as ins
+
+
+
+
+pagina = pgi.PageInstagram(id_admin_insta_page='17841400798532798',graph = facebook.GraphAPI('EAAH22QokW2IBAAZCUMkVilZA7AThDeTGVv1uNcwwHvyLVpn3AwsGNZAxIrZBtGPanvZBZBRu8tm5fZBY9pqSER6CF9eAWl2P48WD4tLIJfievIaiPrAOPLZBtRn5hX5pWwh4EmssnXxU0voS97z3axrvDNxnfbld6i81IYii2avwygZDZD'))
+
+
+
+
+
+
+
+
+
